c9fe1267b4a06298d546755abab8f70363eb76b7
yairHorvitz <156910473+yairHorvitz@users.noreply.github.com>
2024-01-28 17:16:59 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -11,29 +11,35 @@ scanf("%d" "%d", &number1 ,&number2);
 
 
 
-printf("The Prime numbers are:");
-for(int i= number1; i<=number2;i++){
-    if(isPrime(i)){printf("%d",i);}
-    printf(" ");}
-    printf("\n");
 
 printf("The Armstrong numbers are:");
 for(int i= number1; i<=number2;i++){
     if(isArmstrong(i)){printf(" %d",i);}
-    printf(" ");}
+    }
     printf("\n");
 
-printf("The Strong numbers are:");
-for(int i= number1; i<=number2;i++){
-    if(isStrong(i)){printf(" %d",i);}
-    printf(" ");}
-    printf("\n");
-
-printf("The Palindromes are:");
+    printf("The Palindromes are:");
 for(int i= number1; i<=number2;i++){
     if(isPalindrome(i)){printf(" %d",i);}
-    printf(" ");}
+  }
 printf("\n");
+
+
+    printf("The Prime numbers are:");
+for(int i= number1; i<=number2;i++){
+    if(isPrime(i)){printf(" %d",i);}
+    }
+    printf("\n");
+    
+    printf("The Strong numbers are:");
+for(int i= number1; i<=number2;i++){
+    if(isStrong(i)){printf(" %d",i);}
+   }
+    printf("\n");
+
+
+
+
 
 return 0;
 }
